d773c75c5fd1df30b7febfe0314b3cd44128d1b9
egoing <sungwon@gamil.com>
2025-06-27 17:01:46 +0900

    AuthorService

---
+++ author/src/main/java/aivlecloudnative/domain/AuthorService.java
@@ -0,0 +1,28 @@
+@Service
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public void applyAuthor(ApplyAuthorCommand command) {
+        Author author = new Author(command.getAuthorName(), command.getAuthorEmail(), command.getPortfolio());
+        author.apply();  // 내부에서 AuthorApplied 이벤트 발행
+        authorRepository.save(author);
+    }
+
+    public void judgeAuthor(JudgeAuthorCommand command) {
+        Author author = authorRepository.findById(command.getAuthorId())
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+
+        if ("ACCEPT".equalsIgnoreCase(command.getJudgement())) {
+            author.accept();
+        } else {
+            author.reject();
+        }
+
+        authorRepository.save(author);
+    }
+}
