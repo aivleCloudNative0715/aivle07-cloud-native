9ab19593a9cf667ba3c142fcc838fba2df101e1b
LaylaDesktop <crispylemon7120@gmail.com>
2025-06-30 11:21:45 +0900

    feat: 열람신청 요청에 대한 입력값 유효성 검증 추가
    - userId와 bookId에 @NotNull 어노테이션 적용
    - 각각의 필드에 맞춤형 오류 메시지 설정

--- user/src/main/java/aivlecloudnative/domain/RequestContentAccessCommand.java
+++ user/src/main/java/aivlecloudnative/domain/RequestContentAccessCommand.java
@@ -1,10 +1,14 @@
 package aivlecloudnative.domain;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RequestContentAccessCommand {
 
+    @NotNull(message = "아이디를 입력해주세요")
     private Long userId;
+
+    @NotNull(message = "책 아이디를 입력해주세요")
     private Long bookId;
 }
