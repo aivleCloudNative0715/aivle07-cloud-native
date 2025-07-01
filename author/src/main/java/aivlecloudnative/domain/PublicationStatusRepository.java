0db44d481e784ab65cfca20a9f61ee45397cbb57
zheldsjandjfuqek <vance1999@naver.com>
2025-07-01 16:14:51 +0900

    Update PublicationStatusRepository.java

--- author/src/main/java/aivlecloudnative/domain/PublicationStatusRepository.java
+++ author/src/main/java/aivlecloudnative/domain/PublicationStatusRepository.java
@@ -1,14 +1,14 @@
 package aivlecloudnative.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(
     collectionResourceRel = "publicationStatuses",
     path = "publicationStatuses"
 )
 public interface PublicationStatusRepository
-    extends JpaRepository<PublicationStatus, Long> {
+    extends PagingAndSortingRepository<PublicationStatus, Long> {
     List<PublicationStatus> findByManuscriptId(Long manuscriptId);
 }
