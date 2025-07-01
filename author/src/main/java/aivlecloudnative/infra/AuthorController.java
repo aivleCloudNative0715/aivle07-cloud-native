7df57e5841d9869f6faaf466b9e9d19e6969d72c
zheldsjandjfuqek <vance1999@naver.com>
2025-07-01 16:36:42 +0900

    Update AuthorController.java

--- author/src/main/java/aivlecloudnative/infra/AuthorController.java
+++ author/src/main/java/aivlecloudnative/infra/AuthorController.java
@@ -1,20 +1,19 @@
 package aivlecloudnative.infra;
-import aivlecloudnative.domain.*;
 
+import aivlecloudnative.domain.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.transaction.Transactional;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 
 //<<< Clean Arch / Inbound Adaptor
 
 @RestController
-// @RequestMapping(value="/authors")
 @Transactional
 public class AuthorController {
     @Autowired
@@ -23,14 +22,15 @@ public class AuthorController {
     @RequestMapping(value = "/authors/applyauthor",
             method = RequestMethod.POST,
             produces = "application/json;charset=UTF-8")
-    public Author applyAuthor(HttpServletRequest request, HttpServletResponse response 
+    public Author applyAuthor(HttpServletRequest request, HttpServletResponse response
         ) throws Exception {
             System.out.println("##### /author/applyAuthor  called #####");
             Author author = new Author();
-            author.applyAuthor();
+            // author.applyAuthor(); // <-- 삭제
             authorRepository.save(author);
             return author;
     }
+
     @RequestMapping(value = "/authors/judgeauthor",
             method = RequestMethod.POST,
             produces = "application/json;charset=UTF-8")
@@ -38,7 +38,7 @@ public class AuthorController {
         ) throws Exception {
             System.out.println("##### /author/judgeAuthor  called #####");
             Author author = new Author();
-            author.judgeAuthor();
+            // author.judgeAuthor(); // <-- 삭제
             authorRepository.save(author);
             return author;
     }
